3ae0d74008c1461cbf251b527ffcefa34dec610d
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-12-25 21:05:21 +0100

    lagt till CRUD

---
+++ src/fiskdrag/dto/update-fiskdrag.ts
@@ -0,0 +1,6 @@
+export class UpdateFiskDragDto {
+    readonly artikelnummer?: string;
+    readonly Tillverkare?: string;
+    readonly pris?: number;
+    readonly typ?: string;
+  }
--- src/fiskdrag/fiskdrag.service.ts
+++ src/fiskdrag/fiskdrag.service.ts
@@ -4,18 +4,39 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FiskDrag } from './schemas/fiskdrag.schemas';
 import { CreateFiskDragDto } from './dto/create-fiskdrag';
+import { promises } from 'dns';
+import { UpdateFiskDragDto } from './dto/update-fiskdrag';
 
 
 @Injectable()
 export class FiskDragService {
   constructor(@InjectModel(FiskDrag.name) private fiskdragModel: Model<FiskDrag>) {}
 
+  //Skapar ett nytt obejekt.
   async create(createFiskdragDto: CreateFiskDragDto): Promise<FiskDrag> {
-    const createdFiskDrag = new this.fiskdragModel(createFiskdragDto);
-    return createdFiskDrag.save();
+    const createFiskDrag = new this.fiskdragModel(createFiskdragDto);
+    return createFiskDrag.save();
   }
 
+  // retunerar alla objekt som finns i mongodb databasen
   async findAll(): Promise<FiskDrag[]> {
     return this.fiskdragModel.find().exec();
   }
+
+  // Söker reda på ID och uppdaterar dens data. 
+  async update(id: string): Promise<FiskDrag> {
+    return this.fiskdragModel
+    .findByIdAndUpdate({ _id: id }, UpdateFiskDragDto, { new: true })
+    .exec();
+  }
+
+  // kontrollerar mot ID att det finns. det som kontrolleras är det interna som mongodb skapar. finns det så tas det bort. 
+  async delete(id: string): Promise<FiskDrag> {
+    const deleteFiskdrag = await this.fiskdragModel
+      .findByIdAndDelete({ _id: id })
+      .exec();
+    return deleteFiskdrag;
+  }
+
+
 }
